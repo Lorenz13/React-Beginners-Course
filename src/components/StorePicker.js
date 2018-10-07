90145d0b640f4763f2d8e50e5e18ab6f47c85419
Jürgen Lorenz <jurgen@qminderapp.com>
2018-10-07 12:42:38 +0300

    Using helper functions Video #10

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className={'store-selector'}>
         <h2>Please enter your store!</h2>
-        <input type={'text'} required placeholder={'Store Name'}></input>
+        <input type={'text'} required placeholder={'Store Name'} defaultValue={getFunName()}></input>
         <button type={'submit'}>Visit Store</button>
       </form>
     )
