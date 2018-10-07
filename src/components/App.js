3016ddf66b32e0e9019dd4736fe2b01d8523d2f7
Jürgen Lorenz <jurgen@qminderapp.com>
2018-10-07 17:09:06 +0300

    Loading data onClick Video #13

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,25 @@ import React from "react";
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
-  }
+  };
   addFish = (fish) => {
-    const fishes = {... this.state.fishes };
+    const fishes = {... this.state.fishes};
     fishes[`fish-${Date.now()}`] = fish;
     console.log(fishes);
     this.setState({
       fishes: fishes
     })
-  }
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className={'catch-of-the-day'}>
@@ -24,7 +28,7 @@ class App extends React.Component {
           <Header tagline={'Fresh Seafood Market'}/>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
