e1a01885ac59a4cef9dc2caf084be826052c2f1c
Jürgen Lorenz <jurgen@qminderapp.com>
2018-10-07 13:29:24 +0300

    Event handling, refs and this binding Video #11-12

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,17 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <form className={'store-selector'}>
+      <form className={'store-selector'} onSubmit={this.goToStore}>
         <h2>Please enter your store!</h2>
-        <input type={'text'} required placeholder={'Store Name'} defaultValue={getFunName()}></input>
+        <input type={'text'} required ref={this.myInput} placeholder={'Store Name'} defaultValue={getFunName()}></input>
         <button type={'submit'}>Visit Store</button>
       </form>
     )
