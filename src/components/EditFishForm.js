f9f20537ab754f1993b0d6e1e19a258c085227c9
Jürgen Lorenz <jurgen@qminderapp.com>
2018-10-07 22:56:36 +0300

    Little bug fix Video #20

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -11,17 +11,17 @@ class EditFishForm extends React.Component {
 
   render() {
     return (
-      <form className={'fish-edit'} onSubmit={this.createFish}>
-        <input name={'name'} ref={this.nameRef} type={'text'} onChange={this.handleChange} value={this.props.fish.name}/>
-        <input name={'price'} ref={this.priceRef} type={'text'} onChange={this.handleChange} value={this.props.fish.price}/>
-        <select name={'status'} ref={this.statusRef} onChange={this.handleChange} value={this.props.fish.status}>
-          <option className={'available'}>Fresh!</option>
-          <option className={'unavailable'}>Sold Out!</option>
+      <div className={'fish-edit'}>
+        <input name={'name'} type={'text'} onChange={this.handleChange} value={this.props.fish.name}/>
+        <input name={'price'} type={'text'} onChange={this.handleChange} value={this.props.fish.price}/>
+        <select name={'status'} onChange={this.handleChange} value={this.props.fish.status}>
+          <option value={'available'}>Fresh!</option>
+          <option value={'unavailable'}>Sold Out!</option>
         </select>
-        <textarea name={'desc'} ref={this.descRef} onChange={this.handleChange} value={this.props.fish.desc}/>
-        <input name={'image'} ref={this.imageRef} type={'text'} onChange={this.handleChange} value={this.props.fish.image}/>
+        <textarea name={'desc'} onChange={this.handleChange} value={this.props.fish.desc}/>
+        <input name={'image'} type={'text'} onChange={this.handleChange} value={this.props.fish.image}/>
         <button type={'submit'}>+ Add Fish</button>
-      </form>
+      </div>
     )
   }
 }
